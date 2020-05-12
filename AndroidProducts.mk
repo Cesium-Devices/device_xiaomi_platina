361932fc1281c8a315c42619b838afa2878ddefd
1cecreamm <teyengkuroba@gmail.com>
2020-05-13 02:55:49 +0700

    Bootleggers-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -22,9 +22,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_platina.mk
+    $(LOCAL_DIR)/bootleg_platina.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_platina-eng \
-    xtended_platina-userdebug \
-    xtended_platina-user
+    bootleg_platina-eng \
+    bootleg_platina-userdebug \
+    bootleg_platina-user
